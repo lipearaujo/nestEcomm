9e2bfa566ac1769571f3186b8e7187cfcea733f6
lipearaujo <filipecensi@hotmail.com>
2025-01-29 20:01:28 -0300

    change the next.config file

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,7 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://nest-ecomm-nq1i.vercel.app/', // Allowed origins
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed methods
+    credentials: true, // Allow credentials (e.g., cookies)
+    allowedHeaders: 'Content-Type, Accept', // Allowed headers
+  });
   await app.listen(3001);
 }
 bootstrap();
