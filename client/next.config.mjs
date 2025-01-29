5c850932fd2758ed2f11ced85e6e7be1c4dc754f
lipearaujo <filipecensi@hotmail.com>
2025-01-29 19:51:27 -0300

    change the next.config file

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -14,7 +14,7 @@ const nextConfig = {
     return [
       {
         // matching all API routes
-        source: "//products/:path*",
+        source: "/products",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
