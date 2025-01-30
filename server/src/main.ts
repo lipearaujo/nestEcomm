c58c6402b0eb207b0d8adbc5efecf198659054aa
lipearaujo <filipecensi@hotmail.com>
2025-01-30 17:40:09 -0300

    changes

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,9 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.enableCors({
-    origin: 'https://nest-ecomm-nq1i.vercel.app', // Allowed origins
+    origin: [
+      'https://nest-ecomm-nq1i.vercel.app',
+      'https://nest-ecomm-nq1i-git-master-lipearaujos-projects.vercel.app',
+      'https://nest-ecomm-nq1i-2d5lnkkat-lipearaujos-projects.vercel.app/',
+    ], // Allowed origins
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed methods
     credentials: true, // Allow credentials (e.g., cookies)
     allowedHeaders:
