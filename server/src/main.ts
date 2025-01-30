80c4fc05c828262280dd31e19ea30ac291e14dba
lipearaujo <filipecensi@hotmail.com>
2025-01-30 02:51:27 -0300

    changes

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*', // Allowed origins
+    origin: 'https://nest-ecomm-nq1i.vercel.app', // Allowed origins
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed methods
     credentials: true, // Allow credentials (e.g., cookies)
     allowedHeaders:
