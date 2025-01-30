874d9d825676100ef9d08e09ef4106a4f944cef7
lipearaujo <filipecensi@hotmail.com>
2025-01-30 02:54:32 -0300

    changes

--- server/src/users/users.module.ts
+++ server/src/users/users.module.ts
@@ -4,7 +4,7 @@ import { JwtModule } from '@nestjs/jwt';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
-import { PrismaModule } from 'src/prisma/prisma.module';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   imports: [
